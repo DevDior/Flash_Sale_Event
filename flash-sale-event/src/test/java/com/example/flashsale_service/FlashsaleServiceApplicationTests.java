f11bcbd37342b72c2a941b66b5560a4665a83567
Dior <chinup1004@naver.com>
2025-03-06 23:15:01 +0900

    Flash Sale Event Spring Boot 프로젝트 생성

---
+++ flash-sale-event/src/test/java/com/example/flashsale_service/FlashsaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flashsale_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashsaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
