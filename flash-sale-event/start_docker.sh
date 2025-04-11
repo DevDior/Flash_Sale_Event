12963b3d77b76706eb9877c33f3b6d70ae03bd1b
Dior <chinup1004@naver.com>
2025-04-11 20:56:54 +0900

    [infra #5] 로깅 설정 추가, 로그 파일 저장 경로 구성

--- flash-sale-event/start_docker.sh
+++ flash-sale-event/start_docker.sh
@@ -9,10 +9,14 @@ docker rm -f flash-sale-container
 # 이미지 빌드
 docker build -t flash-sale-event .
 
+# logs 디렉터리가 없으면 생성
+mkdir -p ./logs
+
 # 컨테이너 실행 (CPU & 메모리 제한 포함)
 docker run -d \
   --name flash-sale-container \
   --cpus="1" \
   --memory="1024m" \
   -p 8080:8080 \
+  -v $(pwd)/logs:/app/logs \
   flash-sale-event
