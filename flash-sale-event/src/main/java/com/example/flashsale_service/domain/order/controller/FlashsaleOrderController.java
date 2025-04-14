73a8fa910ac9489071db92ef11c8b512ac089839
Dior <chinup1004@naver.com>
2025-04-14 18:37:01 +0900

    [infra #5] 부하테스트 중 서버 로깅 기능 구성

--- flash-sale-event/src/main/java/com/example/flashsale_service/domain/order/controller/FlashsaleOrderController.java
+++ flash-sale-event/src/main/java/com/example/flashsale_service/domain/order/controller/FlashsaleOrderController.java
@@ -2,6 +2,7 @@ package com.example.flashsale_service.domain.order.controller;
 
 import com.example.flashsale_service.domain.order.service.FlashsaleOrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/flashsale")
@@ -22,6 +24,12 @@ public class FlashsaleOrderController {
             orderService.placeOrder(eventId, 1);
             return ResponseEntity.ok("Order placed!");
         } catch (RuntimeException e) {
+            if (!"Sold out!".equals(e.getMessage())) {
+                log.error("[ORDER] 알 수 없는 에러 발생 - eventId={}", eventId, e);
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Unexpected error occurred.");
+            }
+
+            // Sold out!인 경우
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
--- flash-sale-event/src/main/java/com/example/flashsale_service/domain/order/service/FlashsaleOrderService.java
+++ flash-sale-event/src/main/java/com/example/flashsale_service/domain/order/service/FlashsaleOrderService.java
@@ -6,11 +6,13 @@ import com.example.flashsale_service.domain.order.entity.Order;
 import com.example.flashsale_service.domain.order.entity.OrderStatus;
 import com.example.flashsale_service.domain.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class FlashsaleOrderService {
@@ -18,21 +20,32 @@ public class FlashsaleOrderService {
     private final FlashsaleEventRepository flashsaleEventRepository;
     private final OrderRepository orderRepository;
 
+    private static final String SOLD_OUT_MSG = "Sold out!";
+
     @Transactional
     public void placeOrder(Long eventId, int quantity) {
+        log.info("[ORDER] 주문 시도 - eventId={}, quantity={}", eventId, quantity);
+
+        long start = System.currentTimeMillis();
+
         int updated = flashsaleEventRepository.decreaseQuantity(eventId, quantity);
+        long dbLatency = System.currentTimeMillis() - start;
+
         if (updated == 0) {
-            throw new RuntimeException("Sold out!");
+            log.warn("{ORDER] 주문 실패 - 품절 상태 (eventId={})", eventId);
+            throw new RuntimeException(SOLD_OUT_MSG);
         }
+        else {
+            // 주문 생성
+            Order order = Order.builder()
+                    .flashsaleEvent(FlashsaleEvent.builder().id(eventId).build())   // 조회 없이 ID만 설정
+                    .quantity(quantity)
+                    .orderTime(LocalDateTime.now())
+                    .status(OrderStatus.CREATED)
+                    .build();
 
-        // 주문 생성
-        Order order = Order.builder()
-                .flashsaleEvent(FlashsaleEvent.builder().id(eventId).build())   // 조회 없이 ID만 설정
-                .quantity(quantity)
-                .orderTime(LocalDateTime.now())
-                .status(OrderStatus.CREATED)
-                .build();
-
-        orderRepository.save(order);
+            orderRepository.save(order);
+            log.info("[ORDER] 주문 완료 - eventId{}, orderId={}", eventId, order.getId());
+        }
     }
 }
